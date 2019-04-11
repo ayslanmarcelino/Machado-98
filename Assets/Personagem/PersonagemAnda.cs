572fc16a5174f1efe06c0a91c20fab1b16124535
ayslanmarcelino <ayslan_marcelino@outlook.com>
2019-04-11 15:29:43 -0300

    Adding the project

---
+++ Assets/Personagem/PersonagemAnda.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemAnda : MonoBehaviour
+{
+    public float _rotacionar = 100;
+    private Animator _animator;
+    private float _andar = 0;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+
+    void Update(){
+
+        _andar = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+{
+            _andar += 1;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+{
+            _andar = 1;
+        }
+
+        _animator.SetFloat("Andar", _andar);
+
+        this.transform.Rotate(0, (Input.GetAxis("Horizontal") * _rotacionar) * Time.deltaTime, 0);
+    }
+
+}
